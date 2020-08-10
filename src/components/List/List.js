0e22ad65efd3e989c288e7bf262039ef35aed6f0
BasiaM1 <b.murzynowska@wp.pl>
2020-08-10 23:04:12 +0200

    add class component and add props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
-
+import Column from '../Column/Column';
+import PropTypes from 'prop-types';
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    img: PropTypes.string,
+    colTitle1: PropTypes.string,
+    colTitle2: PropTypes.string,
+    colTitle3: PropTypes.string,
+  };
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  };
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} imgSrc={this.props.img} />
+        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+          <Column columnTitle={this.props.colTitle1} />
+          <Column columnTitle={this.props.colTitle2} />
+          <Column columnTitle={this.props.colTitle3} />
+        </div>
       </section>
     );
   }
