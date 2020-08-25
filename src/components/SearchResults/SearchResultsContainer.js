230a100fb1fb19079da36367deb487a4582e4d2d
BasiaM1 <b.murzynowska@wp.pl>
2020-08-25 19:48:33 +0200

    Fix problem with filter cards

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux';
 import SearchResults from './SearchResults';
 import { getCardsForSearchResults } from '../../redux/cardsRedux';
+import { createAction_changeSearchString } from '../../redux/searchStringRedux.js';
 
 const mapStateToProps = (state, props) => ({
-  cards: getCardsForSearchResults(state, props.match.params.title),
+  cards: getCardsForSearchResults(state, props.match.params.searchString),
 });
-
-export default connect(mapStateToProps)(SearchResults);
+const mapDispatchToProps = (dispatch) => ({
+  changeSearchString: (newSearchString) =>
+    dispatch(createAction_changeSearchString(newSearchString)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
